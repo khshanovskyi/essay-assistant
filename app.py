9ed3e54df07fa1679dee2305d77d5e9f3fea74ed
“Khshanovskyi” <pavlo_khshanovskyi@epam.com>
2025-07-25 17:07:20 +0300

    Initial commit

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ SYSTEM_PROMPT = """You are an essay-focused assistant. Respond to every request 
 
 DIAL_URL = os.environ.get('DIAL_URL', 'http://localhost:8080')
 MODEL_NAME = os.environ.get('MODEL_NAME', 'gpt-4o')
+API_VERSION = os.environ.get('API_VERSION', '2025-01-01-preview')
 
 class EssayAssistantApplication(ChatCompletion):
 
@@ -33,10 +34,13 @@ class EssayAssistantApplication(ChatCompletion):
         client: AsyncDial = AsyncDial(
             base_url=DIAL_URL,
             api_key=request.api_key,
-            api_version=""
+            api_version=API_VERSION
         )
 
         with response.create_single_choice() as choice:
+            usr_msg = request.messages[-1].dict(exclude_none=True)
+            print(f"User request: {usr_msg}")
+
             chunks = await client.chat.completions.create(
                 deployment_name=MODEL_NAME,
                 stream=True,
@@ -45,19 +49,26 @@ class EssayAssistantApplication(ChatCompletion):
                         role=Role.SYSTEM,
                         content=SYSTEM_PROMPT,
                     ).dict(exclude_none=True),
-                    request.messages[-1].dict(exclude_none=True)
+                    usr_msg
                 ],
             )
 
+            content = ''
             async for chunk in chunks:
                 if chunk.choices and len(chunk.choices) > 0:
                     delta = chunk.choices[0].delta
                     if delta and delta.content:
                         choice.append_content(delta.content)
+                        content+=delta.content
+
+        print(f"🤖 Result: {content}\n{'-'*100}\n")
 
 
 app: DIALApp = DIALApp()
 app.add_chat_completion(deployment_name="essay-assistant", impl=EssayAssistantApplication())
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=int(os.environ.get('PORT', '5000')))
+    uvicorn.run(
+        app,
+        port=int(os.environ.get('PORT', '5035'))
+    )
